8717e1e3b6860a36f5f9f9e1af900f868cffb361
Sven Steinheißer <sven.steinheisser@googlemail.com>
2012-03-25 01:36:20 +0100

    ignore uploads

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,7 @@ after "deploy", "deploy:cleanup" # keep only the last 5 releases
 namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
