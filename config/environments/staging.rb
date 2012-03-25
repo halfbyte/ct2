1d288450eb5a91f9c3cfd1d498600abb75859412
Sven Steinheißer <sven.steinheisser@googlemail.com>
2012-03-25 10:46:28 +0200

    enable lograge

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -29,6 +29,7 @@ Ct2::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.lograge.enabled = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
