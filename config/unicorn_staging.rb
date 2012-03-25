f4a0a567486593e118aacb547bac613087526f43
Sven Steinheißer <sven.steinheisser@googlemail.com>
2012-03-25 01:01:17 +0100

    foo

--- config/unicorn_staging.rb
+++ config/unicorn_staging.rb
@@ -1,9 +1,17 @@
-root = "/home/cloudtracker/apps/cloudtracker/staging/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+current_path = "/home/cloudtracker/apps/cloudtracker/staging/current"
+shared_path = "/home/cloudtracker/apps/cloudtracker/staging/shared"
+
+working_directory current_path
+
+pid "#{current_path}/tmp/pids/unicorn.pid"
+stderr_path "#{current_path}/log/unicorn.log"
+stdout_path "#{current_path}/log/unicorn.log"
 
 listen "/tmp/unicorn.cloudtracker-staging.sock"
 worker_processes 2
 timeout 30
+
+before_exec do |server|
+  ENV['GEM_HOME'] = ENV['GEM_PATH'] = "#{shared_path}/bundle"
+  ENV['BUNDLE_GEMFILE'] = "#{current_path}/Gemfile"
+end
